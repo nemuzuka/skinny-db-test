2e017affb43011aa02373d8350814717e0747447
Kazumune Katagiri <kkaz123@gmail.com>
2016-08-30 20:15:09 +0900

    anyObjectを使用する際に他のパラメータは固定値の場合、eqマッチャーで設定する

--- src/test/scala/logic/StaffDiLogicImplMockSpec.scala
+++ src/test/scala/logic/StaffDiLogicImplMockSpec.scala
@@ -9,7 +9,6 @@ import org.scalatest._
 import org.scalatest.fixture.FunSpec
 import org.scalatest.mock.MockitoSugar
 import org.mockito.Mockito._
-import org.mockito.Matchers._
 import scalikejdbc.DBSession
 import scalikejdbc.scalatest._
 import skinny._
@@ -40,10 +39,10 @@ class StaffDiLogicImplMockSpec extends FunSpec with AutoRollback with Matchers w
     */
   describe("getStaffDi") {
     it("DB取得") { implicit session =>
-      staffDiLogic.getStaffDi(1L) should be (None)
       val actual = staffDiLogic.getStaffDi(2L).get
       actual.id should be (2L)
       actual.staffName should be ("Mock社員")
+      staffDiLogic.getStaffDi(1L) should be (None)
     }
   }
 
@@ -55,10 +54,10 @@ class StaffDiLogicImplMockSpec extends FunSpec with AutoRollback with Matchers w
     override def configure() = {
       //Mockの定義
       val m = mock[StaffDiDao]
-      val retObj = Option(
+      when(m.findById(org.mockito.Matchers.eq(1L))(org.mockito.Matchers.anyObject())).thenReturn(None)
+      when(m.findById(org.mockito.Matchers.eq(2L))(org.mockito.Matchers.anyObject())).thenReturn(Option(
         StaffDi(id=2L, staffName="Mock社員")
-      )
-      when(m.findById(anyLong())(anyObject())).thenReturn(None, retObj)
+      ))
 
       bind(classOf[StaffDiDao]).toInstance(m)
     }
